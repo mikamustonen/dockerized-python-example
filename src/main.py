68fa876166205fa41e47f1098e7295998bef7cbc
Mika Mustonen <mika.t.mustonen@gmail.com>
2022-08-20 12:19:41 +0300

    Fix splitting quoted command-line arguments to the script

--- src/main.py
+++ src/main.py
@@ -4,6 +4,6 @@ import sys
 # Demonstrate how command-line arguments get passed into the script
 if len(sys.argv) > 1:
     print('Received the following command-line arguments:')
-    print(' '.join(sys.argv[1:]))
+    print(sys.argv[1:])
 else:
     print('No command-line arguments received.')
